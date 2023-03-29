94f8738aadced649befb5252c86d256351b0c459
Naveed Elahi <statix231@hotmail.com>
2023-03-29 18:06:37 +0800

    show total number of quran verses

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import QuranNFTList from "./components/QuranNFTList.vue";
+</script>
+
+<template>
+  <header>
+    <h1>Qur'an NFTs</h1>
+  </header>
+  <main>
+    <QuranNFTList />
+  </main>
+</template>
---
+++ src/components/QuranNFTList.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+import nftService from "../nftService";
+
+const NftService = new nftService();
+
+const totalQuranVerses = ref(0);
+
+onMounted(async () => {
+  totalQuranVerses.value = await NftService.getTotalSupply();
+});
+</script>
+
+<template>
+  <h3>Total Qur'an NFT verses</h3>
+  <div>{{ totalQuranVerses }}</div>
+</template>
